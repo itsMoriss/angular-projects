20e01ccf342e1bd5b4b44cbbeabca57b6fea4ae3
moris mwai <morismwai1@gmail.com>
2024-01-25 17:56:52 +0300

    Add bookmark tile component and styles

--- personal-dashboard/src/app/bookmarks/bookmarks.component.ts
+++ personal-dashboard/src/app/bookmarks/bookmarks.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { BookmarkTileComponent } from "../bookmark-tile/bookmark-tile.component";
 
 @Component({
-  selector: 'app-bookmarks',
-  standalone: true,
-  imports: [],
-  templateUrl: './bookmarks.component.html',
-  styleUrl: './bookmarks.component.scss'
+    selector: 'app-bookmarks',
+    standalone: true,
+    templateUrl: './bookmarks.component.html',
+    styleUrl: './bookmarks.component.scss',
+    imports: [BookmarkTileComponent]
 })
 export class BookmarksComponent {
 
