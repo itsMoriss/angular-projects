6a0309891c4f0512757964b7136353310d212e8c
moris mwai <morismwai1@gmail.com>
2024-02-02 17:54:22 +0300

    Add new component for adding notes and update routing

--- personal-dashboard/src/app/app.routes.ts
+++ personal-dashboard/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes, } from '@angular/router';
 import { BookmarksComponent } from './bookmarks/bookmarks.component';
 import { NotesComponent } from './notes/notes.component';
 import { TodosComponent } from './todos/todos.component';
+import { AddNotesComponent } from './add-notes/add-notes.component';
 
 export const routes: Routes = [
-    { path: 'bookmarks', component:BookmarksComponent },
-    { path: 'notes', component:NotesComponent },
-    { path: 'todos', component:TodosComponent },
+    { path: 'bookmarks', component: BookmarksComponent },
+    { path: 'notes', component: NotesComponent },
+    { path: 'todos', component: TodosComponent },
+    { path: 'notes/add', component: AddNotesComponent}
 ];
--- personal-dashboard/src/app/notes/notes.component.ts
+++ personal-dashboard/src/app/notes/notes.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-notes',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './notes.component.html',
   styleUrl: './notes.component.scss'
 })
