1cefbb0e2809ed7a9a1a97c7e24fec9a29192745
moris mwai <morismwai1@gmail.com>
2024-02-07 18:25:10 +0300

    Form validation inAdd Note View

--- personal-dashboard/src/app/add-notes/add-notes.component.ts
+++ personal-dashboard/src/app/add-notes/add-notes.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { Router, RouterModule } from '@angular/router';
 import { FormsModule, NgForm } from '@angular/forms';
 import { Note } from '../shared/note.model';
@@ -7,7 +8,7 @@ import { NoteService } from '../shared/note.service';
 @Component({
   selector: 'app-add-notes',
   standalone: true,
-  imports: [RouterModule, FormsModule],
+  imports: [RouterModule, FormsModule, CommonModule],
   templateUrl: './add-notes.component.html',
   styleUrl: './add-notes.component.scss'
 })
@@ -16,6 +17,11 @@ export class AddNotesComponent {
   constructor(private notesService: NoteService, private router: Router) { }
 
   onFormSubmit(form: NgForm) {
+
+    if (form.invalid) {
+      return alert('Please enter a title for the note');
+    }
+
     const note = new Note(form.value.title, form.value.content);
     console.log(note);
 
